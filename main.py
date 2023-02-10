2f1e98c37f7413cf77a6ebb9fee7f0945bbcdac6
ikromyusuf <ikromyusupov09@gmail.com>
2023-02-10 23:41:18 +0500

    added instruction

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# create a fuction to get the column of country from the csv file
 def get_country_column(file_name):
     """
     This function takes a filename as input and returns a list of countries
     Args:
         file_name: string
-    Returns:list
+    Returns:
+        list
     """
     return 0
